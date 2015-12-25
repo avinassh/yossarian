feda32608c0a283154d69e43663dec3acbbe61bf
avi <hi@avi.im>
2015-12-25 22:43:33 +0530

    Add `save_book_cover`

--- yossarian/books/views.py
+++ yossarian/books/views.py
@@ -1,6 +1,13 @@
+import os
+import tempfile
+import urllib
+
+import requests
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.http import HttpResponseBadRequest
+from django.conf import settings
+from django.core.files import File
 
 from .models import Book
 from .forms import BookForm
@@ -30,5 +37,20 @@ class BookCreateView(CreateView):
         book.ratings_count = book_details.get('ratings_count')
         book.description = book_details.get('description')
         book.url = book_details.get('url')
+        image_url = book_details.get('image_url')
+        save_book_cover(book, img_name=book.goodreads_id, img_url=image_url)
 
         return super(BookCreateView, self).form_valid(form)
+
+
+def save_book_cover(book, img_name, img_url):
+    if not img_url:
+        return
+    r = requests.get(img_url, stream=True)
+    with tempfile.NamedTemporaryFile() as fp:
+        for block in r.iter_content(1024 * 8):
+            if not block:
+                break
+            fp.write(block)
+        book.cover.save(str(img_name) + '.jpg', File(fp))
+    return True
