82804e0856b9b6983b48972e4780255157e57152
avi <hi@avi.im>
2016-01-02 18:51:40 +0530

    Update `Book` model
    - Rename field `url` to `goodreads_url`
    - Add field `lang_code`
    - Add field `page_count`
    - Change field `goodreads_id` to type PositiveInteger
    - Change field `ratings_count` to type PositiveInteger

--- yossarian/books/models.py
+++ yossarian/books/models.py
@@ -5,15 +5,17 @@ from yossarian.utils import TimeStampMixin
 
 
 class Book(TimeStampMixin):
-    goodreads_id = models.IntegerField(unique=True)
+    goodreads_id = models.PositiveIntegerField(unique=True)
     title = models.CharField(max_length=300)
     authors = models.CharField(max_length=300)
     description = models.TextField(blank=True)
-    url = models.URLField()
+    goodreads_url = models.URLField()
     cover = models.ImageField(upload_to='covers')
+    lang_code = models.CharField(max_length=3, default='eng')
+    page_count = models.PositiveIntegerField(default=1)
 
     average_rating = models.DecimalField(max_digits=3, decimal_places=2)
-    ratings_count = models.IntegerField()
+    ratings_count = models.PositiveIntegerField()
 
     added_by = models.ForeignKey(User)
 
