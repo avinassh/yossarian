f0ba7ffd032e7e6812212f694d2959aface03e62
avi <hi@avi.im>
2016-01-10 13:43:18 +0530

    Update admin for Book and WeeklyBook model with filters

--- yossarian/books/admin.py
+++ yossarian/books/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Book, WeeklyBook
 
-admin.site.register(Book)
-admin.site.register(WeeklyBook)
+
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ('is_reviewed', 'is_contestant')
+
+
+class WeeklyBookAdmin(admin.ModelAdmin):
+    list_filter = ('is_english', 'on_homepage')
+
+admin.site.register(WeeklyBook, WeeklyBookAdmin)
+admin.site.register(Book, BookAdmin)
