0a96d51e87886e6fb4120e0e5540f738a05828e3
avi <hi@avi.im>
2016-01-10 13:54:22 +0530

    Use string notation instead of importing the model

--- yossarian/book_groups/models.py
+++ yossarian/book_groups/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from yossarian.utils import TimeStampMixin
-from yossarian.books.models import Book
 
 
 class BookGroup(TimeStampMixin):
     name = models.CharField(max_length=300)
     is_active = models.BooleanField(default=True)
 
-    book = models.OneToOneField(Book)
+    book = models.OneToOneField('books.Book')
     members = models.ManyToManyField(User, related_name='book_groups')
 
 
