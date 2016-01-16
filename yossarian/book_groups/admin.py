7011d31518f2f41194554571c4afa22a7c36f62c
avi <hi@avi.im>
2016-01-16 18:43:22 +0530

    Delete `Comment` and `CommentVote` models from `book_groups`

--- yossarian/book_groups/admin.py
+++ yossarian/book_groups/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import BookGroup, Progress, Comment
+from .models import BookGroup, Progress
 
 admin.site.register(BookGroup)
 admin.site.register(Progress)
-admin.site.register(Comment)
--- yossarian/book_groups/models.py
+++ yossarian/book_groups/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
-from mptt.models import MPTTModel, TreeForeignKey
 
 from yossarian.utils import TimeStampMixin
 
@@ -23,35 +21,3 @@ class Progress(TimeStampMixin):
 
     class Meta:
         unique_together = ('book_group', 'user')
-
-
-class Comment(MPTTModel, TimeStampMixin):
-    author_name = models.CharField(null=False, max_length=12)
-    raw_comment = models.TextField()
-    html_comment = models.TextField()
-    ups = models.IntegerField(default=0)
-    downs = models.IntegerField(default=0)
-    score = models.IntegerField(default=0)
-
-    author = models.ForeignKey(User)
-    parent = TreeForeignKey('self', related_name='children',
-                            null=True, blank=True, db_index=True)
-    book = models.ForeignKey('books.Book')
-
-    class MPTTMeta:
-        order_insertion_by = ['-score']
-
-
-class CommentVote(TimeStampMixin):
-    value = models.IntegerField(
-        default=0,
-        validators=[
-            MaxValueValidator(1),
-            MinValueValidator(-1)
-        ])
-
-    comment = models.ForeignKey(Comment)
-    user = models.ForeignKey(User)
-
-    class Meta:
-        unique_together = ('comment', 'user')
